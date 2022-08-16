8c8bb24e487cbfa2e1787d1656f7a92854ea4636
Chitranshi <apigeoneer@gmail.com>
2022-08-16 15:18:32 +0530

    chore: topbar ui

---
+++ client/src/components/topbar/Topbar.jsx
@@ -0,0 +1,23 @@
+import "./topbar.css"
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+        <div className="tbleft">
+            <ul>
+                <li className="logo">pmpara</li>
+                <li>Explore</li>
+                <li>Discuss</li>
+            </ul>
+        </div>
+        <div className="tbright">
+            <ul>
+                <li>Join Us!</li>
+                <li className="profile">
+                    <i class="fa fa-user"></i>
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import "./home.css"
+import Topbar from "../../components/topbar/Topbar"
+
+export default function Home() {
+  return (
+    <div>
+        <Topbar/>
+        Home
+    </div>
+  )
+}
