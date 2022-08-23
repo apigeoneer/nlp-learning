0d7471d8eb3dbdbe08709c069b9611bf2712a5ee
Chitranshi <apigeoneer@gmail.com>
2022-08-23 12:12:26 +0530

    chore: making single post's post content & add comment form ui

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import Topbar from "./components/topbar/Topbar";
 import Post from "./pages/post/Post";
 import Home from "./pages/home/Home";
 import JoinUs from "./pages/joinUs/JoinUs";
+import SinglePost from "./pages/singlePost/SinglePost";
 
 function App() {
   return (
     <div className="App">
       <Topbar/>
-      <JoinUs/>
+      <SinglePost/>
     </div>
   );
 }
---
+++ client/src/pages/singlePost/SinglePost.jsx
@@ -0,0 +1,95 @@
+import "./singlePost.css"
+
+export default function SinglePost() {
+  return (
+    <div className="singlePost">
+      <div className="top">
+        <div className="votesAndViews">
+          <div className="votes">
+            <div className="upvotes">
+              <div><i class="fa fa-angle-up"></i></div>
+              <div><span className="numupvotes">34</span></div>
+            </div>
+            <div className="downvotes">
+              <i class="fa fa-angle-down"></i>
+            </div>
+          </div>
+          <div className="views">
+            <div><i class="fa fa-eye"></i></div>
+            <div><span>92</span></div>
+          </div>
+        </div>
+        <div className="topHeading">
+          <div><h1>How to become a PM in 90 days!</h1></div>
+          <div className="topactions">
+            <div><i class="fa fa-share"></i></div>
+            <div><i class="fa fa-bookmark"></i></div>
+          </div>
+        </div>
+        <div className="postDetails"><span>4 hours</span> ago | <span>Chitranxshi</span></div>
+      </div>
+
+      <div className="container">
+        <div className="details">
+          <div className="tags">
+            <ul>
+              <li>interview experience</li>
+              <li>interview question</li>
+              <li>google apm program</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="postContent">
+          <p>
+        LeetCode Discuss is a community where you can share questions and experiences from interviews.
+        Before you post, please try to follow these guidelines:
+        <br />
+        <br />
+        The title should follow this format:
+        <br />
+        Company Name | Stage (Phone, Onsite, OA) | Question name
+        For example, this could look like:
+        <br />
+        Amazon | Phone | Missing number
+        Google | Onsite | Robot Room Cleaner
+        <br />
+        <br />
+        Please post clear and concise descriptions of the problems and make sure your question has not already been posted.
+        If you're including code in your post, please surround the code block with three backticks (```) to make it more readable. Posts that do not follow this guideline may be removed by moderators.
+        Each question should be accompanied by at least one example - the more the better!
+        Please enter the company name in the tags shown below which will help people see your question more easily.
+        <br />
+        <br />
+        Each post should include only one question. If you want to share your overall interview experience, please post in the Interview Experience category.
+        You represent and warrant that any User Content that you submit, post or display through the Services does not infringe or violate any of the copyright, patent, trademark, trade name, trade secrets or any other personal or proprietary rights of any third party.
+        <br />
+        <br />
+        Happy discussing! :)
+          </p>
+        </div>
+      </div>
+
+      <div className="commentSection">
+        <div className="commentDetails">
+          <div><i class="fa fa-comment"></i></div>
+          <div className="numComments">56</div>
+          <div>Comments</div>
+        </div>
+
+        <div className="writeComment">
+          <form className="writeform">        
+            <div className="writeFormGroup">
+              <textarea className="contentinput" placeholder="Write a comment to join the discussion!"></textarea>
+            </div>
+           <div><button className="postbuton" type="submit">Post</button></div>
+          </form>
+        </div>
+
+        <div className="comments">
+          
+        </div>
+      </div>
+    </div>
+  )
+}
